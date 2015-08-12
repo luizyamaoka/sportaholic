ae827a4c3e75e0d9188c734165786e4b52cb3439
Luiz Yamaoka <luiz.yamaoka@gmail.com>
2015-08-11 22:12:56 -0300

    ordering alfabetically uris getAll

--- sportaholic-core/src/java/main/com/sportaholic/dao/UriDao.java
+++ sportaholic-core/src/java/main/com/sportaholic/dao/UriDao.java
@@ -1,8 +1,11 @@
 package com.sportaholic.dao;
 
+import java.util.List;
+
 import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -33,6 +36,16 @@ public class UriDao extends GenericDao<Uri, Integer> {
 		criteria.add(Restrictions.eq("friendlyUri", friendlyUri));
 		Uri uri = (Uri) criteria.uniqueResult();
 		return uri;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Uri> getAll() throws Exception {
+		Session session = null;
+		session = this.sessionFactory.getCurrentSession();
+		Criteria criteria = session.createCriteria(Uri.class);
+		criteria.addOrder(Order.asc("friendlyUri"));
+		List<Uri> objects = criteria.list();
+		return objects;
 	}
 	
 }
